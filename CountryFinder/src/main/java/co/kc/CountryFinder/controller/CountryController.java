201760b918d07de0c3bd402d292d8ca56bc8a33c
Kevin Chung <chungkev7@gmail.com>
2020-01-25 15:21:42 -0500

    Made search results sortable by asc/desc, added comments

--- CountryFinder/src/main/java/co/kc/CountryFinder/controller/CountryController.java
+++ CountryFinder/src/main/java/co/kc/CountryFinder/controller/CountryController.java
@@ -23,11 +23,20 @@ public class CountryController {
 	@Value("${country.key}")
 	private String countryKey;
 	
-	static ArrayList<Country> countryList = new ArrayList<>();
-	static Country randomCountry;
-	
 	RestTemplate rt = new RestTemplate();
 	
+	static ArrayList<Country> countryList = new ArrayList<>();
+	static Country randomCountry;
+	static int nameClickCounter = 0;
+	static int capitalClickCounter = 0;
+	static int populationClickCounter = 0;
+	
+	/**
+	 * Returns a list of countries based on user input request
+	 * 
+	 * @param name: country name text input form from index jsp page
+	 * @return: a list of countries
+	 */
 	@RequestMapping("/search-results")
 	public ModelAndView homePage(@RequestParam("name") String name) {
 		ModelAndView mv = new ModelAndView("search-results");
@@ -52,6 +61,12 @@ public class CountryController {
 		return mv;
 	}
 	
+	/**
+	 * Returns a list of countries in a particular region based on user selection
+	 * 
+	 * @param region: dropdown list of countries
+	 * @return: a list of countries
+	 */
 	@RequestMapping("/region-search")
 	public ModelAndView searchByRegion(@RequestParam("region") String region) {
 		ModelAndView mv = new ModelAndView("search-results");
@@ -71,7 +86,12 @@ public class CountryController {
 		return mv;
 	}
 	
-	
+	/**
+	 * Generates a random country from a particular region for user to guess the population
+	 * 
+	 * @param region: dropdown list of countries
+	 * @return: a random country's population
+	 */
 	@RequestMapping("/random-country-population")
 	public ModelAndView getRandomCountryByRegion(@RequestParam("region") String region) {
 		ModelAndView mv = new ModelAndView("pop-guess");
@@ -94,6 +114,12 @@ public class CountryController {
 		return mv;
 	}
 	
+	/**
+	 * Generates a message to user if user guesses the population correctly or incorrectly
+	 * 
+	 * @param population: compares the user's population guess to the random country
+	 * @return: message if user was correct or not
+	 */
 	@RequestMapping("/pop-guess")
 	public ModelAndView populationGuess(@RequestParam("population") int population) {
 		ModelAndView mv = new ModelAndView("guess-result");
@@ -109,6 +135,12 @@ public class CountryController {
 		return mv;
 	}
 	
+	/**
+	 * Generates a random country from a particular region for user to guess the capital
+	 * 
+	 * @param region: dropdown list of countries
+	 * @return: a random country's capital
+	 */
 	@RequestMapping("/random-country-capital")
 	public ModelAndView getRandomCountryCapital(@RequestParam("region") String region) {
 		ModelAndView mv = new ModelAndView("capital-guess");
@@ -132,6 +164,12 @@ public class CountryController {
 		
 	}
 	
+	/**
+	 * Generates a message to user if user guesses the capital correctly or incorrectly
+	 * 
+	 * @param population: compares the user's capital guess to the random country
+	 * @return: message if user was correct or not
+	 */
 	@RequestMapping("/capital-guess")
 	public ModelAndView capitalGuess(@RequestParam("capital") String capital) {
 		ModelAndView mv = new ModelAndView("guess-result");
@@ -145,6 +183,12 @@ public class CountryController {
 		return mv;
 	}
 	
+	/**
+	 * Generates a random country from all regions for user to guess the population
+	 * 
+	 * @param region: user indication (yes or not to continue)
+	 * @return: a random country's population
+	 */
 	@RequestMapping("/all-country-population")
 	public ModelAndView getRandomCountryByAllPopulation() {
 		ModelAndView mv = new ModelAndView("pop-guess");
@@ -167,6 +211,12 @@ public class CountryController {
 		return mv;
 	}
 	
+	/**
+	 * Generates a random country from all regions for user to guess the capital
+	 * 
+	 * @param region: user indication (yes or not to continue)
+	 * @return: a random country's capital
+	 */
 	@RequestMapping("/all-country-capital")
 	public ModelAndView getRandomCountryByAllCapital() {
 		ModelAndView mv = new ModelAndView("capital-guess");
@@ -189,45 +239,90 @@ public class CountryController {
 		return mv;
 	}
 	
+	/**
+	 * Sorts the country list search results by name
+	 * 
+	 * @return: sorted country list based on their name
+	 */
 	@RequestMapping("/sort-by-name")
 	public ModelAndView sortByName() {
 		ModelAndView mv = new ModelAndView("search-results");
 		
-		Comparator<Country> sortByName = (Country c1, Country c2) -> c1.getName().compareTo(c2.getName());
+		nameClickCounter++;
 		
-		Collections.sort(countryList, sortByName);
+		if (nameClickCounter % 2 != 0) {
+			Comparator<Country> sortByNameDesc = (Country c1, Country c2) -> c2.getName().compareTo(c1.getName());
+			
+			Collections.sort(countryList, sortByNameDesc);
+		} else {
+			Comparator<Country> sortByNameAsc = (Country c1, Country c2) -> c1.getName().compareTo(c2.getName());
+			
+			Collections.sort(countryList, sortByNameAsc);
+		}
 		
 		mv.addObject("results", countryList);
 		
 		return mv; 
 	}
 	
+	/**
+	 * Sorts the country list search results by capital
+	 * 
+	 * @return: sorted country list based on their capital
+	 */
 	@RequestMapping("/sort-by-capital")
 	public ModelAndView sortByCapital() {
 		ModelAndView mv = new ModelAndView("search-results");
 		
-		Comparator<Country> sortByCapital = (Country c1, Country c2) -> c1.getCapital().compareTo(c2.getCapital());
+		capitalClickCounter++;
 		
-		Collections.sort(countryList, sortByCapital);
+		if (capitalClickCounter % 2 != 0) {
+			Comparator<Country> sortByCapitalAsc = (Country c1, Country c2) -> c1.getCapital().compareTo(c2.getCapital());
+		
+			Collections.sort(countryList, sortByCapitalAsc);
+		} else {
+			Comparator<Country> sortByCapitalDesc = (Country c1, Country c2) -> c2.getCapital().compareTo(c1.getCapital());
+			
+			Collections.sort(countryList, sortByCapitalDesc);
+		}
 		
 		mv.addObject("results", countryList);
 				
 		return mv;
 	}
 	
+	/**
+	 * Sorts the country list search results by population
+	 * 
+	 * @return: sorted country list based on their population
+	 */
 	@RequestMapping("/sort-by-population")
 	public ModelAndView sortByPopulation() {
 		ModelAndView mv = new ModelAndView("search-results");
 		
-		Comparator<Country> sortByPopulation = (Country c1, Country c2) -> Integer.compare(c1.getPopulation(), c2.getPopulation());
+		populationClickCounter++;
 		
-		Collections.sort(countryList, sortByPopulation);
+		if (populationClickCounter % 2 != 0) {
+			
+			Comparator<Country> sortByPopulationDesc = (Country c1, Country c2) -> Integer.compare(c2.getPopulation(), c1.getPopulation());
+		
+			Collections.sort(countryList, sortByPopulationDesc);
+		} else {
+			Comparator<Country> sortByPopulationAsc = (Country c1, Country c2) -> Integer.compare(c1.getPopulation(), c2.getPopulation());
+			
+			Collections.sort(countryList, sortByPopulationAsc);
+		}
 		
 		mv.addObject("results", countryList);
 		
 		return mv;
 	}
 	
+	/**
+	 * Clears the country list if user goes back to the main page
+	 * 
+	 * @return: an empty countryList 
+	 */
 	@RequestMapping("/main-page")
 	public ModelAndView homePage() {
 		ModelAndView mv = new ModelAndView("index");
@@ -237,6 +332,11 @@ public class CountryController {
 		return mv;
 	}
 	
+	/**
+	 * Adds countries to countryList
+	 * 
+	 * @param response: json data from api request
+	 */
 	public static void addtoCountryList(ResponseEntity<Country[]> response) {
 		for (Country c : response.getBody()) {
 			countryList.add(c);
